b785ecda19b233545d56e7a554841bcfbbb8a2ca
Joshua Lemkin <joshualemkin@Joshuas-MacBook-Pro.local>
2021-04-22 01:11:20 -0500

    Initial commit

---
+++ src/main/java/com/lem/app/rest/Repo/UserRepo.java
@@ -0,0 +1,7 @@
+package com.lem.app.rest.Repo;
+
+import com.lem.app.rest.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+}
